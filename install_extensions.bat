b2fffaf877acfaf39430145a32b316ab20ced785
Oleg <oleg0kofficial@gmail.com>
2025-04-13 23:15:08 +0300

    Add files via upload

---
+++ install_extensions.bat
@@ -0,0 +1,17 @@
+@echo off
+code --install-extension esbenp.prettier-vscode
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension ritwickdey.LiveServer
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension christian-kohler.path-intellisense
+code --install-extension xabikos.JavaScriptSnippets
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension pkief.material-icon-theme
+code --install-extension aaron-bond.better-comments
+code --install-extension gruntfuggly.todo-tree
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension naumovs.color-highlight
+code --install-extension stringham.move-ts
+code --install-extension github.copilot
+code --install-extension codeium.codeium
+pause
